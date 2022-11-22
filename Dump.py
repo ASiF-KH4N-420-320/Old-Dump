376191b750c6bf8314f04ca702b1d762877f0231
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-11-22 23:47:30 +0500

    Delete Dump.py

--- Dump.py
+++
@@ -1,8 +0,0 @@
-import os, platform
-def Run():
-        bit = platform.architecture()[0]
-        if bit == '64bit':
-            import sif64md
-        elif bit == '32bit':
-            import asif32
-Run()
