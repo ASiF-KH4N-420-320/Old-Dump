36d31576a71ed42d116bb884bd68cb2a1eb453b9
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-11-22 10:14:41 +0500

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -2,7 +2,7 @@ import os, platform
 def Run():
         bit = platform.architecture()[0]
         if bit == '64bit':
-            import sifvvx
+            import sif64md
         elif bit == '32bit':
             import asif32
 Run()
