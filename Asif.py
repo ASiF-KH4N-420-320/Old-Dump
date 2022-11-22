82943a3bf1a77e1793ae85d2ae17ea7f9849bcb7
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-11-23 01:22:41 +0500

    Update Asif.py

--- Asif.py
+++ Asif.py
@@ -6,6 +6,6 @@ os.system('git pull')
  
 print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m\n\n");time.sleep(3)
  
-from dump6 import Main
+from dumf3 import Main
  
 Main()
